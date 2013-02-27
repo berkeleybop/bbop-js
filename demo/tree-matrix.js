a3571e9c7fef689b2aaba316efd79afb9d98054e
Mitch Skinner <git@arctur.us>
2013-02-26 21:44:30 -0800

    make it work in IE9 and IE8; move tree out of table

--- demo/tree-matrix.js
+++ demo/tree-matrix.js
@@ -10,9 +10,10 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
                        matrix, cell_renderer, config) {
     var default_config = {
         initial_tree_width: 600,
-        cell_width: 16,
+        cell_width: 22,
         header_height: 35,
-        scale_drag_linewidth: 4
+        scale_drag_linewidth: 4,
+	table_cell_border: 1
     };
 
     if( "object" == typeof config ){
@@ -23,9 +24,9 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
 
     this.tree_width = this.config.initial_tree_width;
 
-    this.parent = (parent instanceof HTMLElement
-                       ? parent
-                       : document.getElementById(parent));
+    this.parent = ( ( "string" == typeof parent )
+		    ? document.getElementById(parent)
+		    : parent );
     if (this.parent === undefined) {
         throw "can't find parent element " + parent;
     }
@@ -33,27 +34,29 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
     this.phylo_table = document.createElement("table");
     this.phylo_table.className = "tree_matrix";
     this.phylo_table.style.cssText = [
-        "position: absolute;",
-        "top: 0px;",
-        "left: 0px;",
-        "width: 100%"
+        "position: absolute",
+        "top: 0px",
+        "left: " + ( this.tree_width
+		     + this.config.scale_drag_linewidth ) + "px",
+        "width: " + ( this.config.cell_width
+		      * column_descriptors.length ) + "px"
     ].join(";") + ";";
     this.parent.appendChild(this.phylo_table);
 
     this.tree_parent = document.createElement("div");
     this.tree_parent.style.cssText = [
         "position: absolute",
-        "top: 0px",
+        "top: " + this.config.header_height + "px",
         "left: 0px",
         "width: " + this.tree_width + "px"
     ].join(";") + ";";
-    this.phylo_table.appendChild(this.tree_parent);
+    this.parent.appendChild(this.tree_parent);
         
     this.tree = new bbop.render.phylo.divrenderer(this.tree_parent, {
         leaf_font: "Helvetica, Arial, sans-serif",
-        box_height: 22,
+        box_height: 18,
         leaf_border: 0,
-        leaf_padding: 5,
+        leaf_padding: 3,
         box_spacing: 4
     });
 
@@ -78,10 +81,6 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
     this.tree.sort_tree("ladderize_up");
     //create one row for the matrix header row
     var row = document.createElement("tr");
-    this.top_left_cell = document.createElement("th");
-    this.top_left_cell.style.width = this.tree_width + "px";
-    this.top_left_cell.style.border = "none";
-    row.appendChild(this.top_left_cell);
     for (var i = 0; i < column_descriptors.length; i++) {
         var cell = document.createElement("th");
         cell.title = column_descriptors[i];
@@ -92,9 +91,6 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
     var leaves = this.tree.leaves();
     for( var li = 0; li < leaves.length; li++ ){
         row = document.createElement("tr");
-        var first_col = document.createElement("td");
-        first_col.style.border = "none";
-        row.appendChild(first_col);
 
         for (var col = 0; col < matrix[li].length; col++) {
             var cell = document.createElement("td");
@@ -114,18 +110,16 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
         "table.tree_matrix tr { ",
         "  height: " + this.tree.row_height + "px;",
         "}",
-        "table.tree_matrix tr:nth-child(odd)  { background-color:#eee; }",
-        "table.tree_matrix tr:nth-child(even) { background-color:#fff; }",
+        "table.tree_matrix tr:nth-child(even)  { background-color:#eee; }",
+        "table.tree_matrix tr:nth-child(odd) { background-color:#fff; }",
         "table.tree_matrix td { ",
-        "  border: 1px solid black;",
+        "  border: " + this.config.table_cell_border + "px solid black;",
         "}",
         "table.tree_matrix th { ",
-        "  border: 1px solid black;",
+        "  border: " + this.config.table_cell_border + "px solid black;",
         "}",
         "table.tree_matrix tr:hover td { background-color: #bef195; }"
     ].join("\n");
-
-    this.tree_parent.style.top = this.config.header_height + "px";
 
     this.set_styles(this.default_stylesheet);
 
@@ -133,6 +127,7 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
                                  + this.config.header_height ) + "px";
 
     this.parent.style.width = ( this.tree_width
+				+ this.config.scale_drag_linewidth
                                 + ( this.config.cell_width
                                     * column_descriptors.length ) ) + "px";
 
@@ -161,17 +156,20 @@ tree_matrix = function(parent, phylo_graph, column_descriptors,
     this.scale_drag_elem.appendChild(scale_drag_handle);
 
     var self = this;
-    this.phylo_table.appendChild(this.scale_drag_elem);
+    this.parent.appendChild(this.scale_drag_elem);
     $( this.scale_drag_elem ).draggable({
         axis: "x",
         drag: function( event, ui ) {
-            self.tree_width = ui.position.left;
+            self.tree_width = Math.max(0, ui.position.left);
             self.tree_parent.style.width = self.tree_width + "px";
+            self.phylo_table.style.left = 
+		( self.tree_width
+		  + self.config.scale_drag_linewidth ) + "px";
             self.parent.style.width =
                 ( self.tree_width
+		  + self.config.scale_drag_linewidth
                   + ( self.config.cell_width
                       * column_descriptors.length ) ) + "px";
-            self.top_left_cell.style.width = ( self.tree_width ) + "px";
             self.tree.width_changed( self.tree_width );
         }
     });
